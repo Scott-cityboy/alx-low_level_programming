03a724a015605ab5854e18a56df00e1648778a1a
Scott-cityboy <scottimbiaka@gmail.com>
2023-04-17 16:12:55 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,14 +8,13 @@
  * @owner: owner of dog
  */
 
-struct dog
+struct dog(char *d)
 {
 	char *name;
 	float age;
 	char *owner;
 };
 
-struct dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
