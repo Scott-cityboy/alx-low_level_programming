c344bba9d92869016336d3267d33a18930413e7f
Scott-cityboy <scottimbiaka@gmail.com>
2023-04-17 16:37:34 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @owner: owner of dog
  */
 
-struct dog(char *d)
+struct dog(*d);
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
