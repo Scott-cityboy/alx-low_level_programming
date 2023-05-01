6feaef3733c4865f545fdcf02cb305aaa5d345a0
Scott-cityboy <scottimbiaka@gmail.com>
2023-05-01 13:46:10 -0700

    0x0D-preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,10 +3,11 @@
 /**
  * main - prints the name of the file
  *
- * Return: Always 0(Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
+
